1d3a6b1737fc1e0084ff921c4d6fbc7c4c7d14cc
lukriv <lukriv@centrum.cz>
2012-09-23 14:07:32 +0200

    - added reference counting and GameSystem and GameObject libs

---
+++ WordOfFlat/GameSystem/refobject.h
@@ -0,0 +1,17 @@
+#ifndef __REFERENCED_OBJECT_H__
+#define __REFERENCED_OBJECT_H__
+
+
+#include "../../extern/include/wx/defs.h"
+
+
+class IRefObject {
+protected:
+	virtual ~IRefObject(){};
+
+public:
+	void addRef() = 0;
+	wxInt32 release() = 0;
+}
+
+#endif //__REFERENCED_OBJECT_H__
