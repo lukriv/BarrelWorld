c2af57aee554b253e231b51354764ffcc4300bc5
lukriv <lukriv@centrum.cz>
2012-10-01 22:18:53 +0200

    - improve builds - make make project buildable

--- WordOfFlat/GameObjects/gameobj.h
+++ WordOfFlat/GameObjects/gameobj.h
@@ -1,13 +1,29 @@
 #ifndef __GAME_OBJ_H__
 #define __GAME_OBJ_H__
 
+#include "refobject.h"
+
 enum GameObjectType {
 	GAME_TYPE_TREE
 };
 
-class IGameObject {
+class IGameObject : public IRefObject {
 public:
 	virtual GameObjectType GetType() = 0;
+	virtual bool IsStatic() = 0;
+	virtual bool IsMoveable() = 0;
+	virtual bool IsDestroyable() = 0;
+	virtual bool IsDrawable() = 0;
+	
+	/*! \brief Draw an object
+	 * 
+	 * If object is not drawable, this method do nothing.
+	 * 
+	 * \retval true Drawing object was successful.
+	 * \retval false Drawing object failed.
+	 */
+	virtual bool Draw() = 0;
 }
+
 
 #endif //__GAME_OBJ_H__
