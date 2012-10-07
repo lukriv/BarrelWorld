b01c336178cf77d862a81d111807120f810d6b6a
lukriv <lukriv@centrum.cz>
2012-10-07 15:03:17 +0200

    - add GameObjectSrv project with server objects

---
+++ WordOfFlat/GameObjectsSrv/gamesrvobj.h
@@ -0,0 +1,12 @@
+#ifndef __GAME_SERVER_OBJECT_H__
+#define __GAME_SERVER_OBJECT_H__
+
+#include "../GameSystem/refobject.h"
+
+class IGameObjectSrv : public IRefObject {
+public:
+	virtual GameObjectType GetType() = 0;
+	virtual wxInt32 GetId() = 0;
+}
+
+#endif //__GAME_SERVER_OBJECT_H__
