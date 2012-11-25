6c19f1d9ad58bc312695b4ba07b2ca41908a5aa8
lukriv <lukriv@centrum.cz>
2012-11-25 15:32:52 +0100

    - do some changes to objects

--- WordOfFlat/GameObjectsSrv/gamesrvobjimpl.h
+++ WordOfFlat/GameObjectsSrv/gamesrvobjimpl.h
@@ -3,12 +3,23 @@
 
 #include "gamesrvobj.h"
 
+
 class GameObjectSrvImpl : public IGameObjectSrv {
 private:
+	struct FixtureSet{
+		b2FixtureDef m_fixtureDef;
+		b2Fixture* m_pFixture;
+		FixtureSet() : m_pFixture(NULL){}
+	};
+private:
 	b2BodyDef m_bodyDef;
+	wxVector<b2Shape*> m_pShapeVec;
+	wxVector<FixtureSet> m_pFixtureVec;
+	b2Body* m_pBody;
+	b2World* m_pOwner;
 public:
 	virtual GameObjectType GetType() = 0;
 	virtual wxInt32 GetId() = 0;
-}
+};
 
 #endif //__GAME_SERVER_OBJECT_IMPL_H__
