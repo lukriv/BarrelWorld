364a0b0050343c545237c6855171b1cea7d9d140
lukriv <lukriv@centrum.cz>
2013-05-31 01:10:59 +0200

    - add part implementation of flatworld client

---
+++ WordOfFlat/WorldCli/flatworldcliimpl.cpp
@@ -0,0 +1,33 @@
+#include "flatworldcliimpl.h"
+
+GameErrorCode GameFlatWorldClient::AddDrawableObject(wxInt32 objectId, IGameObject* pObject)
+{
+}
+
+GameErrorCode GameFlatWorldClient::AddShape(GameShapeId shapeId, sf::Shape* pShape)
+{
+}
+
+GameErrorCode GameFlatWorldClient::AddTexture(GameTextureId texId, sf::Texture* pTexture)
+{
+}
+
+bool GameFlatWorldClient::DrawScreen()
+{
+}
+
+IGameObject* GameFlatWorldClient::GetObject(wxInt32 objId)
+{
+}
+
+sf::Shape* GameFlatWorldClient::GetShape(GameShapeId shapeId)
+{
+}
+
+sf::Texture* GameFlatWorldClient::GetTexture(GameTextureId texId)
+{
+}
+
+GameFlatWorldClient::~GameFlatWorldClient()
+{
+}
