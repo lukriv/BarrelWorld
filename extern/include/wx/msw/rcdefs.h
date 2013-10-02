8eda73530d0c864c014cf2d2f3977e31e756ad5a
lukriv <lukriv@centrum.cz>
2013-10-03 00:00:12 +0200

    - upgrade to wxWidgets version 2.9.5

--- extern/include/wx/msw/rcdefs.h
+++ extern/include/wx/msw/rcdefs.h
@@ -1,6 +1,17 @@
-#ifndef _WX_RCDEFS_H
-#define _WX_RCDEFS_H
+# 1 "..\\..\\include\\wx\\msw\\genrcdefs.h"
+# 1 "<command-line>"
+# 1 "..\\..\\include\\wx\\msw\\genrcdefs.h"
+# 12 "..\\..\\include\\wx\\msw\\genrcdefs.h"
+ #ifndef _WX_RCDEFS_H
+ #define _WX_RCDEFS_H
 
-#define WX_CPU_X86
 
-#endif
+
+
+
+
+ #define wxUSE_RC_MANIFEST 1
+# 36 "..\\..\\include\\wx\\msw\\genrcdefs.h"
+ #define WX_CPU_X86
+# 47 "..\\..\\include\\wx\\msw\\genrcdefs.h"
+ #endif
