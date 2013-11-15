78975b50f19aeb3edee8623f1b49433c40390262
lukriv <lukriv@centrum.com>
2013-11-15 06:45:53 -0800

    - add new consoleplane game project

---
+++ shmcpp/consoleplane/consoleWindow.h
@@ -0,0 +1,21 @@
+#ifndef __CONSOLE_WINDOW_WRAPPER_H__
+#define __CONSOLE_WINDOW_WRAPPER_H__
+
+
+
+
+class ConsoleWindowWrapper {
+	
+public:
+	ConsoleWindowWrapper() {}
+	~ConsoleWindowWrapper() {}
+	
+	bool Initialize(int width, int height);
+	
+	
+	
+};
+
+
+
+#endif //__CONSOLE_WINDOW_H__
