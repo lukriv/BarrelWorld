00e5bac22b7026f53b927f0190338076bc7a94fc
lukriv <lukriv@centrum.com>
2013-11-18 03:33:33 -0800

    - add console window implementation

--- shmcpp/consoleplane/main.cpp
+++ shmcpp/consoleplane/main.cpp
@@ -3,16 +3,39 @@
 #include <iostream>
 #include "consoleWindow.h"
 
+#define WIDTH 	80
+#define HEIGHT 	80
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
 	ConsoleWindowWrapper console;
-	if(!console.Initialize(0,0))
+	if(!console.Initialize())
 	{
 		cout << "ConsoleWindowWrapper::Initialize error" << endl;
 		return 1;
 	}
+	
+	if(!console.SetFontSize(CONSOLE_FONT_SIZE_16x12)) 
+	{
+		cout << "ConsoleWindowWrapper::SetFontSize error" << endl;
+		return 1;
+	}
+	
+	if(!console.SetConsoleBufferSize(WIDTH, HEIGHT))
+	{
+		cout << "ConsoleWindowWrapper::SetConsoleBufferSize error" << endl;
+		return 1;		
+	}
+	
+	if(!console.SetConsoleWindowSize(WIDTH, HEIGHT))
+	{
+		cout << "ConsoleWindowWrapper::SetConsoleWindowSize error" << endl;
+		return 1;		
+	}
+	
+	console.WriteConsoleInfo();
 	
 	return 0;
 }
