84c3d45c489ae31425a82af282f7b57dd3e9bb59
lukriv <lukriv@centrum.cz>
2013-11-24 16:28:22 +0100

    - implement simple movable avatar

--- shmcpp/consoleplane/engine.h
+++ shmcpp/consoleplane/engine.h
@@ -4,9 +4,21 @@
 
 #include "consoleWindow.h"
 
+enum EKeyState {
+	KEY_STATE_NONE 		= 0,
+	KEY_STATE_LEFT 		= 1,
+	KEY_STATE_RIGHT		= 1<<1,
+	KEY_STATE_UP		= 1<<2,
+	KEY_STATE_DOWN		= 1<<3,
+	KEY_STATE_FIRE		= 1<<4
+	
+};
+
+
 struct Character {
 	ConsoleCoord pos;
 };
+
 
 
 class ConsoleGameEngine {
@@ -14,14 +26,26 @@ class ConsoleGameEngine {
 	ConsoleCoord m_viewFieldMax;
 	Character m_avatar;
 public:
-	ConsoleGameEngine(): m_viewFieldMin(0,0)
-						,m_viewFieldMax(0,0) {}
+	ConsoleGameEngine() 
+	{
+		m_viewFieldMin.x = m_viewFieldMin.y = 0;
+		m_viewFieldMax.x = m_viewFieldMax.y = 0;
+	}
 	~ConsoleGameEngine() {}
 	
 	bool WriteMargin(ConsoleWindowWrapper &console, const ConsoleCoord &leftTopPos, const ConsoleCoord &insizeSize );
-	bool Play();
+	bool Play(ConsoleWindowWrapper &console);
 	
-	
+private:
+	void SetUpFlag(unsigned int flag, bool set, unsigned int &flags)
+	{
+		if(set)
+		{
+			flags |= flag;
+		} else {
+			flags &= (~flag);
+		}
+	}	
 	
 };
 
