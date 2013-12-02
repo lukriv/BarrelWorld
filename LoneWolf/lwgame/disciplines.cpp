4e2922b98d47a89f8e107a421443823e12a82c94
lukriv <lukriv@centrum.cz>
2013-12-03 00:39:57 +0100

    - add disciplines manager implementation

--- LoneWolf/lwgame/disciplines.cpp
+++ LoneWolf/lwgame/disciplines.cpp
@@ -1,9 +1,41 @@
 #include "disciplines.h"
 
+const EDisciplines DisciplinesManager::UNKNOWN_DISC = Discipline();
+
 bool DisciplinesManager::AddDisciplineDesc(EDisciplines type, const wxString& title, wxString& desc)
 {
+	TDisciplinesMap::iterator iter;
+	iter = m_disciplineDescMap.find(type);
+	if(iter == m_disciplineDescMap.end())
+	{
+		//add new map item
+		std::pair<TDisciplinesMap::iterator, bool> insertPair;
+		insertPair = m_disciplineDescMap.insert(type);
+		if(insertPair.second)
+		{
+			insertPair.first->second.m_type = type;
+			insertPair.first->second.m_title = title;
+			insertPair.first->second.m_desc = desc;
+		} else {
+			return false;
+		}
+	} else {
+		iter->second.m_type = type;
+		iter->second.m_title = title;
+		iter->second.m_desc = desc;
+	}
+	
+	return true;
 }
 
 Discipline& DisciplinesManager::GetDiscipline(EDisciplines type)
 {
+	TDisciplinesMap::iterator iter;
+	iter = m_disciplineDescMap.find(type);
+	if(iter != m_disciplineDescMap.end())
+	{
+		return iter->second;
+	}
+	
+	return UNKNOWN_DISC;	
 }
--- LoneWolf/lwgame/disciplines.h
+++ LoneWolf/lwgame/disciplines.h
@@ -6,6 +6,11 @@ struct Discipline {
 	EDisciplines m_type;
 	wxString m_title;
 	wxString m_desc;
+public:
+	Discipline() : m_type(DISCIPLINE_UNKNOWN) {}
+	Discipline(EDisciplines discType) : m_type(discType) {}
+	Discipline(EDisciplines discType, const wxString &title, const wxString &desc);
+	~Discipline() {}
 };
 
 
@@ -14,8 +19,11 @@ struct Discipline {
 class DisciplinesManager {
 	typedef std::map<EDisciplines, Discipline> TDisciplinesMap;
 	typedef std::pair<EDisciplines, Discipline> TDisciplinesMapPair;
+	
+	const EDisciplines UNKNOWN_DISC;
 private:
 	TDisciplinesMap m_disciplineDescMap;
+	
 public:
 	DisciplinesManager() {}
 	~DisciplinesManager() {}
