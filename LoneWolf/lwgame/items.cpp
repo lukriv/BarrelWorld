708845411c49f98a2d9b500bc22562b7a5a4cd70
lukriv <lukriv@centrum.cz>
2013-12-09 01:04:42 +0100

    - add implementation of item manager

--- LoneWolf/lwgame/items.cpp
+++ LoneWolf/lwgame/items.cpp
@@ -1,13 +1,57 @@
 #include "items.h"
 
-ItemAndWeaponManager::AddBagItem(EBagItems type, const wxString& title, const wxString& desc)
+const Item ItemAndWeaponManager::UNKNOWN_ITEM = Item();
+
+bool ItemAndWeaponManager::AddBagItem(EBagItems type, const wxString& title, const wxString& desc)
 {
+	return AddItem(type, title, desc, m_bagItemsMap);
 }
 
-ItemAndWeaponManager::AddSpecialItem(ESpecialItems type, const wxString& title, const wxString& desc)
+bool ItemAndWeaponManager::AddSpecialItem(ESpecialItems type, const wxString& title, const wxString& desc)
 {
+	return AddItem(type, title, desc, m_specialItemsMap);
 }
 
-ItemAndWeaponManager::AddWeapon(EWeapons type, const wxString& title, const wxString& desc)
+bool ItemAndWeaponManager::AddWeapon(EWeapons type, const wxString& title, const wxString& desc)
 {
+	return AddItem(type, title, desc, m_weaponsMap);
+}
+
+Item& ItemAndWeaponManager::GetBagItem(EWeapons type)
+{
+	return GetItem(type, m_bagItemsMap);
+}
+
+Item& ItemAndWeaponManager::GetSpecialItem(EWeapons type)
+{
+	return GetItem(type, m_specialItemsMap);
+}
+
+Item& ItemAndWeaponManager::GetWeapon(EWeapons type)
+{	
+	return GetItem(type, m_weaponsMap);
+}
+
+Item& ItemAndWeaponManager::AddItem(wxInt32 type, const wxString& title, const wxString& desc, TItemMap& itemMap)
+{
+	Item item;
+	item.m_id = type;
+	item.m_title = title;
+	item.m_desc = desc;
+	std::pair<TItemMap::iterator, bool> insertResult;
+	insertResult = itemMap.insert(item);
+	
+	return insertResult.second;
+}
+
+Item& ItemAndWeaponManager::GetItem(wxInt32 type, TItemMap& itemMap)
+{
+	TItemMap::iterator iter;
+	iter = itemMap.find(type);
+	if(iter == itemMap.end())
+	{
+		return UNKNOWN_ITEM;
+	}
+	
+	return item->second;
 }
