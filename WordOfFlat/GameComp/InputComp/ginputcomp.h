dcf0ff65d843c630cbdf1dc50fee1a9dad92ba7c
lukriv <lukriv@centrum.cz>
2014-04-30 00:28:38 +0200

    - added new input component implementation first part

--- WordOfFlat/GameComp/InputComp/ginputcomp.h
+++ WordOfFlat/GameComp/InputComp/ginputcomp.h
@@ -9,6 +9,75 @@ enum InputActions {
 	INPUT_ACTION_DEFEND	= 1<<3
 };
 
+class ControlStruct
+{
+public:
+	enum StateFlags {
+		INPUT_ACTION_NONE =						0 ,
+	    INPUT_ACTION_LEFT = 				(1<<0),
+	    INPUT_ACTION_RIGHT = 				(1<<1),
+	    INPUT_ACTION_UP =					(1<<2),
+	    INPUT_ACTION_DOWN = 				(1<<3),
+		INPUT_ACTION_FORWARD =				(1<<4),
+		INPUT_ACTION_BACKWARD =				(1<<5),
+		INPUT_ACTION_JUMP =					(1<<6),
+		INPUT_ACTION_CROUCH =				(1<<7),
+	    INPUT_ACTION_ATTACK =	 			(1<<8),
+	    INPUT_ACTION_DEFEND =				(1<<9),
+		INPUT_ACTION_USE =					(1<<10),
+	    
+	};
+private:
+	wxDword m_state; //control state vector
+	wxDword m_clicked; //note thate state was processed yet - reseted by release
+//	wxInt32 m_lastMousePosition[2];
+//	wxInt32 m_lastMousePositionPressed[2];
+	wxInt32 m_mouseWheelDelta;
+public:
+	inline void Set(StateFlags flag)
+	{
+		m_clicked |= flag;
+		m_state |= flag;
+	}
+	
+	inline void Release(StateFlags flag)
+	{
+		m_state &= ~((wxDword) flag)
+	}
+
+	inline bool IsPressed(StateFlags flag) {
+		return ((m_state & flag) != 0);
+	}
+
+	inline bool WasClicked(StateFlags flag) {
+		return ((m_clicked & flag) != 0);
+	}
+
+	
+
+//	inline const sf::Vector2i& GetLastMousePosition() {
+//		return m_lastMousePosition;
+//	}
+//	inline void SetLastMousePosition(const sf::Vector2i& mousePos) {
+//		m_lastMousePosition = mousePos;
+//	}
+//
+//	inline const sf::Vector2i& GetLastMousePositionPressed() {
+//		return m_lastMousePositionPressed;
+//	}
+//	inline void SetLastMousePositionPressed(const sf::Vector2i& mousePos) {
+//		m_lastMousePositionPressed = mousePos;
+//	}
+
+	inline const wxInt32 GetMouseWheelDelta() {
+		return m_mouseWheelDelta;
+	}
+	inline void SetMouseWheelDelta(wxInt32 mouseWheelDelta) {
+		m_mouseWheelDelta = mouseWheelDelta;
+	}
+
+};
+
 
 class InputComponent {
 	
@@ -29,8 +98,9 @@ public:
 	
 	wxDword GetActiveActions();
 	
-	void SetActionMask(wxDword actionMask);
-	wxDword GetActionMask();
+	void SetMoveUp(bool move);
+	void SetMoveDown(bool move);
+	void SetMoveLeft(bool move);
 	
 };
 
