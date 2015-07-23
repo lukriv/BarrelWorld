2d58c91a50887b4a029b8045247fe096d6ddf6f2
Lukas Krivsky <lukriv@centrum.cz>
2015-07-23 22:53:44 +0200

    - try to continue with component implementation

--- WordOfFlat/GameComp/LogicComp/glogicfreecammgr.cpp
+++ WordOfFlat/GameComp/LogicComp/glogicfreecammgr.cpp
@@ -10,4 +10,11 @@ LogicFreeCameraManager::~LogicFreeCameraManager()
 
 GameErrorCode LogicFreeCameraManager::CreateComponent(wxDword compId, LogicFreeCamera*& pNewComponent)
 {
+	RefObjSmPtr<LogicFreeCamera> spInputComp;
+	FWG_RETURN_FAIL(GameNewChecked(spInputComp.OutRef()));
+	FWG_RETURN_FAIL(spInputComp->Initialize(m_pInputSystem));
+	
+	FWG_RETURN_FAIL(InsertToMap(compId, spInputComp));
+	
+	return FWG_NO_ERROR;
 }
